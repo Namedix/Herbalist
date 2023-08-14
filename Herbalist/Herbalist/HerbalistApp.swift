c1733c8f4724741ad6626558df22697aef4572fb
Bartek <namedix@gmail.com>
2023-08-14 12:26:11 +0300

    Initial project setup

---
+++ Herbalist/Herbalist/HerbalistApp.swift
@@ -0,0 +1,17 @@
+//
+//  HerbalistApp.swift
+//  Herbalist
+//
+//  Created by Bartek Pichalski on 14/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct HerbalistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
