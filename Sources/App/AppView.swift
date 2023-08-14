0f6adc1292e25a88772d4bc1510e20536db8ce01
Bartek <namedix@gmail.com>
2023-08-14 12:50:18 +0300

    Add app target

---
+++ Sources/App/AppView.swift
@@ -0,0 +1,20 @@
+//
+//  File.swift
+//  
+//
+//  Created by Bartek Pichalski on 14/08/2023.
+//
+
+import SwiftUI
+
+struct AppView: View {
+    var body: some View {
+        Text("Test")
+    }
+}
+
+struct AppView_Preview: PreviewProvider {
+    static var previews: some View {
+        AppView()
+    }
+}
